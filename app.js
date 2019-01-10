2abe8817f043096f3aa1ab4e429682554434b81e
lizkavalski <lizkavalski@gmail.com>
2019-01-09 17:18:35 -0800

    did some editing mostly in JS and the other files

--- app.js
+++ app.js
@@ -1,50 +1,77 @@
   
 alert('Nice to meet you I am going to ask you a couple of questions');
+//question 1
+var user = prompt('What is my name?');
+  if (user === 'Liz' || user ==='Elizabeth'){
+      alert ('You are correct')
+    }else{ 
+        alert('No it Liz or Elizabeth')}
 
-var user = prompt('What is your name?');
-console.log('name',user);
+console.log('name', user);
+//question 2 (loop)
+for(i=0; i< 3; i++){
+    var place = prompt('Where do you think I am from?' );
+        if (place === 'Washington'){
+            alert( 'You are Correct')
+        } else { 
+        alert ('Try again')}  
+     } 
+console.log('place',place); 
+// question 3 (frist yes or no question)
+var ready = prompt("Alright  the next question is a yes/no question. Please answer with Yes or No. Are you ready?" );
+console.log('Ready', ready)
 
-var place = prompt('Where are you from?' );
-console.log('place',place);   
-
-var answer = prompt("Alright " +user  + " the next question is a yes/no question. Please answer with Yes or No. Are you ready?" );
-console.log('Ready', answer)
-
-    if (answer === "Y" || answer==="y" ||answer=== "Yes" ||answer=== "yes" ){
-        alert ("Alright lets go " + user +".")
+    if (ready === 'Yes' || ready ==='Y' ){
+        alert ('Alright lets go .')
     } else{ 
-        alert("I think you can do it "+ user + " ." )}
- 
-var toDoCodeing = prompt(" Another yes/no question: Are you excited to do coding?")
+        alert('I think you can do it .' )}
+//question 3 (second y/n)
+var toDoCodeing = prompt(' Another yes/no question: I am excited to do coding?')
         console.log('excited to do code', toDoCodeing)
 
-    if (answer === "Y" || answer==="y" ||answer=== "Yes" ||answer=== "yes" ){
-        alert('I am too')
+    if (toDoCodeing === "Y" || toDoCodeing==="y" ||toDoCodeing=== "Yes" ||toDoCodeing=== "yes" ){
+        alert('I am excited')
     } else{ 
-        alert("I think you are excited "+ user + " ." )}
-
-var dog = prompt("Another yes/no questions: Do you have a dog?")
+        alert("I am excited "+ user + " ." )}
+//question 4 (thrid y/n)
+var dog = prompt("Another yes/no questions: Do I have a dog?")
     console.log('Dog', dog)
-        if (answer === "Y" || answer==="y" ||answer=== "Yes" ||answer=== "yes" ){
-            alert('I have a dog too')
-    } else{ 
-    alert('Dogs make great snuggle buddies' )}
-
-var numDog = prompt ('how many dogs do you have?')
+        if (dog === "Y" || dog==="y" ||dog === "Yes" ||dog=== "yes" ){
+            alert('I have a dog ')
+         } else{ 
+            alert(' I do have a dogs and they make great snuggle buddies' )}
+//question 5(make into number loop)
+for(var i = 0; i<3; i++){
+        var numDog = parseInt(prompt ('how many dogs do I have?'))
+            if (numDog === 2){
+                alert('You are correct')
+            }
+            if(numDog < 2){
+                 alert('too low')
+            }
+            if (numDog > 2)
+                { alert('too high')
+            }
+          //  if (numDog !== ) {alert('a number plase')}
+        }
     console.log('number of dogs', numDog)
-        if (answer === 2 || answer === 'two'|| answer=== "Two"){alert('I have two dogs too.')
-        } else{
-            alert('That too sad')}
-
-var wantDragon = prompt('Would you like a pet dragon?')
+      
+//question 6 (fifth y/n)
+var wantDragon = prompt('Would I like a pet dragon?')
     console.log('dragon',wantDragon)
-
-var car= prompt('Do you drive?')
+// question 7 (sixth y/n)
+var car= prompt('Do I drive?')
         console.log('drive', car)
-    if (answer === "Y" || answer==="y" ||answer=== "Yes" ||answer=== "yes" ){
-        alert("I do too "+ user + " ." )}
+    if (car === "Y" || car==="y" ||car=== "Yes" ||car=== "yes" ){
+        alert("Yes I do "+ user + " ." )}
     else {
-        alert('that too sad')}
-
-var typeCar= prompt('What type of car?')
+        alert('I do drive')}
+//question 8 (make into a loop)
+var typeCar= prompt('What type of car do I drive?')
         console.log('Make of car', typeCar)
+// question 9 (arrary w/loop)
+var backGrund = [childcare, food, werehouse]
+for(var i= 0; y<backGrund.length; i++){
+    var vocation= prompt ('What was my previous job background?' )
+    console.log(backGrund[i])
+}
